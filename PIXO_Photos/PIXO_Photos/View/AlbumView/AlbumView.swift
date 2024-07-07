f3dd6a65a52017752d40b039e6cc664f074881dc
jmindeveloper <jmin.developer@gmail.com>
2024-07-07 11:52:00 +0900

    [feat] MainTabbar, PhotoStorage, Album View 생성

---
+++ PIXO_Photos/PIXO_Photos/View/AlbumView/AlbumView.swift
@@ -0,0 +1,16 @@
+//
+//  AlbumView.swift
+//  PIXO_Photos
+//
+//  Created by J_Min on 7/7/24.
+//
+
+import SwiftUI
+
+struct AlbumView: View {
+    
+    var body: some View {
+        Color.red
+    }
+    
+}
---
+++ PIXO_Photos/PIXO_Photos/View/MainTabbarController.swift
@@ -0,0 +1,12 @@
+//
+//  MainTabbarController.swift
+//  PIXO_Photos
+//
+//  Created by J_Min on 7/7/24.
+//
+
+import UIKit
+
+final class MainTabbarController: UITabBarController {
+    
+}

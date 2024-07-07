fe79fd4ecedf28c1584877bf141974171d743ad0
jmindeveloper <jmin.developer@gmail.com>
2024-07-07 12:38:24 +0900

    [feat] tabbarController item 설정

--- PIXO_Photos/PIXO_Photos/View/MainTabbarController.swift
+++ PIXO_Photos/PIXO_Photos/View/MainTabbarController.swift
@@ -6,7 +6,43 @@
 //
 
 import UIKit
+import SwiftUI
 
 final class MainTabbarController: UITabBarController {
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureTabbar()
+    }
+    
+    private func setTabbarControllerItem(view: some View, title: String, image: UIImage, isNavigation: Bool) -> UIViewController {
+        let viewController: UIViewController
+        if isNavigation {
+            viewController = UINavigationController(rootViewController: UIHostingController(rootView: view))
+        } else {
+            viewController = UIHostingController(rootView: view)
+        }
+        
+        viewController.tabBarItem.title = title
+        viewController.tabBarItem.image = image
+        
+        return viewController
+    }
+    
+    private func configureTabbar() {
+        let photoVC = setTabbarControllerItem(
+            view: PhotoStorageView(),
+            title: "보관함",
+            image: UIImage(systemName: "photo.stack") ?? UIImage(),
+            isNavigation: false
+        )
+        let albumVC = setTabbarControllerItem(
+            view: AlbumView(),
+            title: "앨범",
+            image: UIImage(systemName: "square.stack.fill") ?? UIImage(),
+            isNavigation: false
+        )
+        
+        setViewControllers([photoVC, albumVC], animated: true)
+    }
 }

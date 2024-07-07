5b58d0ba3a2c0a7d6ecba42d59217980c0b1c727
jmindeveloper <jmin.developer@gmail.com>
2024-07-07 09:29:55 +0900

    [setting] project init

---
+++ PIXO_Photos/PIXO_Photos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PIXO_Photos
+//
+//  Created by J_Min on 7/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

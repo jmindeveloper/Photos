2f4ca1dcd76b5ef8328fd0a1c07b2e60d0d09e00
jmindeveloper <jmin.developer@gmail.com>
2024-07-08 13:27:19 +0900

    [feat] collectionView layout 설정

--- PIXO_Photos/PIXO_Photos/View/Detail/PhotoDetailCollectionViewController.swift
+++ PIXO_Photos/PIXO_Photos/View/Detail/PhotoDetailCollectionViewController.swift
@@ -6,5 +6,51 @@
 //
 
 import UIKit
+import SnapKit
 
-
+final class PhotoDetailCollectionViewController: UIViewController {
+    
+    // MARK: - ViewProperties
+    private lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: horizontalSwipeLayout())
+        collectionView.backgroundColor = .black
+//        collectionView.delegate = self
+        collectionView.alwaysBounceVertical = false
+        
+        return collectionView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - setSubViews
+    private func setSubViews() {
+        view.backgroundColor = .black
+        view.addSubview(collectionView)
+        
+        setConstraints()
+    }
+    
+    private func setConstraints() {
+        collectionView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    // MARK: - CollectionViewLayout
+    private func horizontalSwipeLayout() -> UICollectionViewCompositionalLayout {
+        let viewFrame = UIScreen.main.bounds
+        
+        let itemSize = NSCollectionLayoutSize(widthDimension: .absolute(viewFrame.width), heightDimension: .absolute(viewFrame.height))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .absolute(viewFrame.width), heightDimension: .absolute(viewFrame.height))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .groupPaging
+                
+        return UICollectionViewCompositionalLayout(section: section)
+    }
+}
